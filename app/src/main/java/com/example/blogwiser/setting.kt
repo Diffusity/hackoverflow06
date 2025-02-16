2d94508226c3d1429d4ec6ff22da691d94bc1683
Diffusity <diffusity@gmail.com>
2025-02-16 15:02:43 +0530

    eigth commit

--- app/src/main/java/com/example/blogwiser/setting.kt
+++ app/src/main/java/com/example/blogwiser/setting.kt
@@ -1,10 +1,12 @@
 package com.example.blogwiser
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.button.MaterialButton
 
 class setting : Fragment() {
     override fun onCreateView(
@@ -12,7 +14,19 @@ class setting : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val v:ViewGroup
+        val logout:MaterialButton
+        val myblog:MaterialButton
         v=inflater.inflate(R.layout.fragment_setting, container, false) as ViewGroup
+        logout=v.findViewById(R.id.logout)
+        logout.setOnClickListener(View.OnClickListener {
+            val intent: Intent = Intent(context,intro::class.java)
+            startActivity(intent)
+        })
+        myblog=v.findViewById(R.id.myblogs)
+        myblog.setOnClickListener(View.OnClickListener {
+            val intent: Intent = Intent(context,myblogs::class.java)
+            startActivity(intent)
+        })
         return v
     }
 }
